06b66dd9e4ff3cce9eff1194b1cf4f74812d6bd0
gopikrishnab <gopikrishnab@ideyalabs.com>
2023-07-06 01:36:26 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python3.6
+WORKDIR /LoginProject
+COPY . .
+RUN "python3 -m venv venv"
+RUN "source venv/bin/activate"
+RUN "pip install -r requirements.txt"
+# RUN "python3 manage.py makemigrations"
+RUN "python3 manage.py migrate"
+CMD ["python3","manage.py runserver"]
---
+++ app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+        print("username and password are : ", username, password)
+        if username in  User.objects.filter(username=username):
+            return Response(f'{username} is already exists!')
+        elif username and password:
+            user = User.objects.create(username=username, first_name=username)
+            user.set_password(password)
+            if "is_superuser" in request.data and request.data.get("is_superuser",False) == True:
+                user.is_superuser = True
+                user.is_staff = True
+            user.save()
+            if user:
+                return Response(f"User created with username : {username}")
+        return Response({"details":"Please enter valid username and password!"})
