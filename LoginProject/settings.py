43589f20dbc268bd485bb0413fda86f563898d91
gopikrishnab <gopikrishnab@ideyalabs.com>
2023-07-07 14:29:44 +0530

    Added django cors

--- LoginProject/settings.py
+++ LoginProject/settings.py
@@ -43,6 +43,14 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:8000",
+        "http://127.0.0.1:9000",
+    ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
